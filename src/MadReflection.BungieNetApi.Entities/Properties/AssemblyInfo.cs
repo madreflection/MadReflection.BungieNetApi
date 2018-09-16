46442c2123afc6208bb68b64909892f05cabb154
Benn Benson <madreflection@live.com>
2018-09-15 23:03:38 -0700

    Cleaned up metadata and prepared for publishing.

--- src/MadReflection.BungieNetApi.Entities/Properties/AssemblyInfo.cs
+++ src/MadReflection.BungieNetApi.Entities/Properties/AssemblyInfo.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Bungie.net API Entities Library")]
+
+[assembly: Guid("a6513a75-1b73-4193-b98f-de497891c018")]
