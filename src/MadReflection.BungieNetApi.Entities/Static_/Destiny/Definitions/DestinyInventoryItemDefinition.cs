81ffa602f56c1c1391264733543b20b706a388e0
Benn Benson <madreflection@live.com>
2018-10-21 00:31:15 -0700

    Added some undocumented properties and removed one.

---
+++ src/MadReflection.BungieNetApi.Entities/Static_/Destiny/Definitions/DestinyInventoryItemDefinition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace BungieNet.Destiny.Definitions
+{
+	partial class DestinyInventoryItemDefinition
+	{
+		[Undocumented]
+		[JsonProperty("acquireRewardSiteHash")]
+		public uint AcquireRewardSiteHash { get; set; }
+
+		[Undocumented]
+		[JsonProperty("acquireUnlockHash")]
+		public uint AcquireUnlockHash { get; set; }
+	}
+}
